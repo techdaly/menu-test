55abb537fda2e2a7bdcf19a03cf34bfb1e46dbd9
Amanda Daly <techdaly@gmail.com>
2016-07-25 14:47:14 -0700

    added all colors and patterns to css, beginning js

---
+++ menu-test/js/scripts.js
@@ -0,0 +1,9 @@
+var colors = ["red", "blue", "white", "yellow", "black", "green", "purple", "mulberry", "ash", "brown", "sky", "amaranth", "ermine", "vair"];
+
+
+$(".button-right").click(function(event) {
+  event.preventDefault();
+  alert("you have clicked");
+
+  
+})
